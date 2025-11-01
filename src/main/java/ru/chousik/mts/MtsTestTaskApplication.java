5ac139dae9639aaed8c8b809298829e79c3cf198
chousik <Zaxap2018@yandex.ru>
2025-11-01 17:10:46 +0300

    init commit

---
+++ src/main/java/ru/chousik/mts/MtsTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.chousik.mts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtsTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/chousik/mts/MtsTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chousik.mts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
